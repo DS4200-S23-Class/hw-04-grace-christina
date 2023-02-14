c3cf7ed995e8b28a05768f38ce1c2c1899abbd17
christinahe <he.chri@northeastern.edu>
2023-02-13 19:37:43 -0500

    changes

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
+// completes actions when points are clicked
 function click() {
-    let col = document.getElementBy('lin');
+    // changes border
+    this.classList.toggle('border');
 
-    col.classList.add('red');
+    // gets coordinates to print out
+    let xCoor = (this.getAttribute("cx") - 5) / 40
+    let yCoor = 10 - ((this.getAttribute("cy") - 5) / 40)
+
+    // prints out text
+    let newText = "The last point clicked was: (" + xCoor + ", " + yCoor + ")";
+    let buttonDiv = document.getElementById("lastPt");
+    buttonDiv.innerHTML = newText
 
 }
-document.getElementById('btn').addEventListener('click', submitClicked)
 
-function mouseover() {
-    let circ = document.getElementByTagName('circle');
+// sets point id
+pointId = 5
+// adds new points based on input
+function submitPoint() {
+    // gets new coordinate points
+    let xCoor = (document.getElementById("x-coor").value * 40) + 5
+    let yCoor = ((10 - document.getElementById("y-coor").value) * 40) + 5
 
-    circ.classList.toggle('orange');
+    // adds point to svg and resets Event Listeners
+    let plot = document.getElementById("plot");
+    plot.innerHTML += "<circle class='point' id='" + pointId + "' cx='" + xCoor + "' cy='" + yCoor + "' r='7'></circle>"
+    addListeners()
 
+    // updates point id
+    pointId += 1
 }
-document.getElementById('circ').addEventListener('mouseover', mouseover)
-function mouseout() {
-    let circ = document.getElementById('circ');
+// Event Listener for point adder button
+document.getElementById('addPoint').addEventListener('click', submitPoint)
 
-    circ.classList.remove('orange');
+// functino to add event listeners for all points
+function addListeners() {
+    let pts = Array.from(document.getElementsByClassName("point"))
+    for (let i = 0; i < pts.length; i++) {
+        pts[i].addEventListener('click', click)
+    }
 }
-document.getElementById('circ').addEventListener('mouseout', mouseout)
+// adds event listeners initially
+addListeners()
 
